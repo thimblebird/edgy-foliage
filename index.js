931aff464dd81575ca207fec8e97f8aaa904afe4
CREEATION <tcreeten@gmail.com>
2021-05-28 01:16:11 +0200

    + minecraft:cactus model

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const processor = new Edgy({
   elements_directory: './src/elements/',
 })
 
-processor.buildElements()
+processor.buildElements((_elements) => {
+  // todo: overwrite block models support
+  fs.copyFileSync(
+    './src/blocks/cactus.json',
+    './build/assets/minecraft/models/block/cactus.json'
+  )
+})
 
 fs.copyFileSync('./src/pack.mcmeta', './build/pack.mcmeta')
 fs.copyFileSync('./src/pack.png', './build/pack.png')
