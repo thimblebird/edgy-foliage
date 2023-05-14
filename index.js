41049b6be599adade433625187988263255a0cdf
thimblebird <thimblebird@creeation.de>
2023-05-14 19:40:19 +0200

    feat: added project image builder

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Edgy, { LOG } from "./edgy.js";
 
 import fs from "fs-extra";
 import chalk from "chalk";
+import jimp from "jimp";
 
 const pkg = fs.readJsonSync("./package.json");
 const buildName = `${pkg.name}-${pkg.version}-${pkg.config.minecraft_version}`;
@@ -25,6 +26,28 @@ processor.buildElements((_elements) => {
   processor.buildArchive(buildName, (output_filepath) => {
     LOG.console();
     LOG.info(`🎁 Archive output: ${chalk.green(output_filepath)}`);
-    LOG.console();
+
+    // build higher resolution images from pack icon
+    const image_sizes = [400];
+
+    image_sizes.forEach((image_size) => {
+      const image_output_filepath = `${processor.build_directory}icon-${image_size}x${image_size}.png`;
+
+      jimp
+        .read(processor.getPackDir("pack.png"))
+        .then((icon) => {
+          return icon
+            .resize(image_size, image_size, jimp.RESIZE_NEAREST_NEIGHBOR)
+            .write(image_output_filepath);
+        })
+        .then(() => {
+          LOG.info(
+            `🎨 Project image output: ${chalk.green(image_output_filepath)}`
+          );
+        })
+        .catch((err) => {
+          LOG.error(err);
+        });
+    });
   });
 });
