448430e5dff752cd481b707b62e7a3bb5fa1e906
thimblebird <thimblebird@creeation.de>
2026-01-11 17:44:55 +0100

    adds project icon generator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Edgy, { LOG } from "./edgy.js";
 
 import fs from "fs-extra";
 import chalk from "chalk";
+import { Jimp, ResizeStrategy } from "jimp";
 
 const pkg = fs.readJsonSync("./package.json");
 const buildName = `${pkg.name}-${pkg.version}-${pkg.config.minecraft_version}`;
@@ -9,14 +10,14 @@ const buildName = `${pkg.name}-${pkg.version}-${pkg.config.minecraft_version}`;
 const processor = new Edgy({
   build_directory: "./build/",
   build_pack_directory: `./build/resourcepacks/${buildName}/`,
-  elements_directory: "./src/elements/",
+  elements_directory: "./src/elements/"
 });
 
 processor.buildElements((_elements) => {
   // todo: overwrite block models support
   fs.copySync(
     "./src/blocks/cactus.json",
-    processor.getAssetsDir(`minecraft/models/block/cactus.json`),
+    processor.getAssetsDir(`minecraft/models/block/cactus.json`)
   );
 
   fs.copySync("./src/pack.mcmeta", processor.getPackDir("pack.mcmeta"));
@@ -26,7 +27,7 @@ processor.buildElements((_elements) => {
   // @see https://mods.jfronny.dev/Respackopts/
   fs.copySync(
     "./src/respackopts.json5",
-    processor.getPackDir("respackopts.json5"),
+    processor.getPackDir("respackopts.json5")
   );
   fs.copySync("./src/respackopts/", processor.getPackDir("/assets/"));
 
@@ -34,5 +35,32 @@ processor.buildElements((_elements) => {
     LOG.console();
     LOG.info(`🎁 Archive output: ${chalk.green(output_filepath)}`);
     LOG.console();
+  });
+
+  // build higher resolution images from pack icon
+  const image_sizes = [400];
+
+  image_sizes.forEach((image_size) => {
+    const image_output_filepath = `${processor.build_directory}icon-${image_size}x${image_size}.png`;
+
+    Jimp.read(processor.getPackDir("pack.png"))
+      .then((icon) => {
+        return icon
+          .resize({
+            w: image_size,
+            h: image_size,
+            mode: ResizeStrategy.NEAREST_NEIGHBOR
+          })
+          .write(image_output_filepath);
+      })
+      .then(() => {
+        LOG.console();
+        LOG.info(
+          `🎨 Project image output: ${chalk.green(image_output_filepath)}`
+        );
+      })
+      .catch((err) => {
+        LOG.error(err);
+      });
   });
 });
